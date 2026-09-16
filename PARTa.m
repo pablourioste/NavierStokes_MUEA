56d8c3930270a4204026cfebc5815793f8648705
marcantich <marc.antich@gmail.com>
2026-09-16 19:08:37 +0200

    Update PARTa.m
    Soy un puto noob nose com va. NO FEU CAS ALS CANVIS MEUS

--- PARTa.m
+++ PARTa.m
@@ -1,6 +1,6 @@
 %% Aerodinamica, Mecanica de Vol i Orbital - Project: Part A
 % Author: Pablo Urioste // Marc Antich // Martí Esquerda
-%Hola ya me va
+%Soc noob en github estic provant
 % Last version: 16/09/2026 --18:07
 
 main();
